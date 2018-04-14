3a821c06f0d5a243619ac7dbe421796088c2f651
Alexandru Ardelean <ardeleanalex@gmail.com>
2018-04-14 15:28:12 +0300

    pages/user: add basic page stuff (qotd & pic)
    At this point, we're done with the core stuff of this app.
    Just need to resolve stuff with the API and then this should work.
    
    Atm, we're showing Quote-of-the-day & Pic, which is the cornerstone
    if this app.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -1,13 +1,47 @@
 <template>
   <q-page class="row justify-center">
+    <q-list no-border>
+      <q-item>
+        <h5>{{ currentDate }}</h5>
+      </q-item>
+
+      <q-item>
+        <p class="caption">{{ qotd }}</p>
+      </q-item>
+
+      <q-item>
+        <img :src="imgSrc" alt="no-image" />
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <style>
+h5 {
+  margin: 0;
+}
 </style>
 
 <script>
 export default {
-  name: 'PageUser'
+  name: 'PageUser',
+  computed: {
+    userFromUrl () {
+      let pathname = window.location.pathname
+      while (pathname[0] === '/') {
+        pathname = pathname.slice(1)
+      }
+      return pathname
+    },
+    currentDate () {
+      return (new Date()).toDateString()
+    },
+    qotd () {
+      return 'Dummy Quote'
+    },
+    imgSrc () {
+      return 'https://upload.wikimedia.org/wikipedia/commons/c/c4/PM5544_with_non-PAL_signals.png'
+    }
+  }
 }
 </script>
