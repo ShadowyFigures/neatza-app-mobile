f6be7780768119443891726850bcc5ca518fbc18
Alexandru Ardelean <ardeleanalex@gmail.com>
2018-04-14 11:57:36 +0300

    router,pages/user: add blank page for the user view and routes
    With this logic, the user can now navigate to a user page (which is currently
    empty, but will be populated in the next commits).
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

---
+++ src/pages/user.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="row justify-center">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageUser'
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,6 @@
 
+import users from '../assets/users'
+
 const defaultRoute = {
   path: '/',
   component: () => import('layouts/default'),
@@ -14,6 +16,15 @@ const catchRoute = {
 
 const routes = []
 routes.push(defaultRoute)
+users.forEach(user => {
+  routes.push({
+    path: '/' + user.name.toLowerCase(),
+    component: () => import('layouts/default'),
+    children: [
+      { path: '', component: () => import('pages/user') }
+    ]
+  })
+})
 routes.push(catchRoute)
 
 export default routes
