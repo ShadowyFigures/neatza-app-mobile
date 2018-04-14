17f0dca4088f22619d843ef0ee8f9b396aedfb26
Alexandru Ardelean <ardeleanalex@gmail.com>
2018-04-14 11:59:12 +0300

    assets/user.js: add hard-coded user list
    This app has a limited audience, so we hard-code the user list in the app
    source code.
    Later on (when we have a server API) we could make this a bit more dynamic.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

---
+++ src/assets/users.js
@@ -0,0 +1,14 @@
+export default [
+  { name: 'Ali' },
+  { name: 'Andi' },
+  { name: 'Bibi' },
+  { name: 'Cheap' },
+  { name: 'John' },
+  { name: 'Mihaela' },
+  { name: 'Nae' },
+  { name: 'Ovi' },
+  { name: 'Puiu' },
+  { name: 'Sandu' },
+  { name: 'Teo' },
+  { name: 'Tulia' }
+]
