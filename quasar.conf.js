3702e4170f94400ea8048ed63870f4454ddb956e
Alexandru Ardelean <ardeleanalex@gmail.com>
2018-04-14 11:23:32 +0300

    quasar.conf.js: add QLayoutFooter component
    That's where tabs will be placed.
    
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,6 +47,7 @@ module.exports = function (ctx) {
       components: [
         'QLayout',
         'QLayoutHeader',
+        'QLayoutFooter',
         'QLayoutDrawer',
         'QPageContainer',
         'QPage',
