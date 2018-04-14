4040c5d4f7e392bb7a59cd23a132a24051ab69c0
Alexandru Ardelean <ardeleanalex@gmail.com>
2018-04-14 12:01:20 +0300

    layouts/default: add footer links for user navigation
    Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -12,12 +12,27 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-layout-footer>
+      <q-tabs color="secondary">
+        <q-route-tab slot="title" v-for="(user, index) in users" :key=index
+         :to="`/${user.name.toLowerCase()}`" replace :label="`${user.name}`"
+        />
+      </q-tabs>
+    </q-layout-footer>
   </q-layout>
 </template>
 
 <script>
+import users from '../assets/users'
+
 export default {
-  name: 'LayoutDefault'
+  name: 'LayoutDefault',
+  data () {
+    return {
+      users
+    }
+  }
 }
 </script>
 
