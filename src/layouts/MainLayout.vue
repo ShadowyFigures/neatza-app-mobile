76a026c8b95c7e6b997cdc883f0f098e3b94617e
Alexandru Ardelean <ardeleanalex@gmail.com>
2020-11-04 08:40:41 +0200

    wip

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,15 +13,24 @@
       <router-view />
     </q-page-container>
 
+    <q-footer>
+      <q-tabs color="secondary">
+        <q-route-tab slot="title" v-for="(user, index) in users" :key=index
+         :to="`/${user.name.toLowerCase()}`" replace :label="`${user.name}`"
+        />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
 
 <script>
+import users from '../assets/users'
 
 export default {
   name: 'MainLayout',
   data () {
     return {
+      users
     }
   }
 }
